37fdb49fff429ad9edc50f4df1626f94f6c2ba66
jtneedels <44848904+jtneedels@users.noreply.github.com>
2019-01-07 15:15:32 -0800

    6 DOF

--- EntryVehicleDynamics.jl
+++ EntryVehicleDynamics.jl
@@ -1,19 +1,19 @@
-# Right now just 3 DOF model, need to add rotational dynamics
-# Placeholders for force coefficients
 # Need subroutine to calculate angle of attack
+
 function entry_vehicle!(dX,X,p,t)
 # Define Constants
-    m = 1000.0; # Vehicle Mass [kg]
+    m = 100.0; # Vehicle Mass [kg]
     mu = 3.986E14; # Standard Grav Parameter (Earth) [m3s-2]
     rho_0 = 1.225; # First Atmospheric Paremeter [kg/m3]
     H = 8.434E3; # Atmospheric Scale Height [m]
-    A = 1; #Area [m2]
+    A = 10; #Area [m2]
     rho = rho_0*exp(-X[1]/H); #Exponential Atmosphere Model
-
+    J =  [1 0 0; 0 1 0; 0 0 1];
+    Torque = [1; 1; 1];
     C_A = 1.6; # Values at 0 angle of attack
     C_S = -.1;
     C_N = 0;
-    C_T = .5; # Dummy value for coefficient
+    C_T = 1; # Dummy value for coefficient
 
     # Velocity
     dX[1] = X[8];
@@ -21,19 +21,17 @@ function entry_vehicle!(dX,X,p,t)
     dX[3] = X[10];
 
     # Angular Velocity
-    dX[4] = 0;
-    dX[5] = 0;
-    dX[6] = 0;
-    dX[7] = 0;
+    dX[4] = .5*X[5]*X[13] - .5*X[6]*X[12] + .5*X[7]*X[11];
+    dX[5] = -.5*X[4]*X[13] + .5*X[6]*X[11] + .5*X[7]*X[12];
+    dX[6] = .5*X[4]*X[12] - .5*X[5]*X[11] + .5*X[7]*X[13];
+    dX[7] = -.5*X[4]*X[11] - .5*X[5]*X[12] - .5*X[6]*X[13];
 
     # Acceleration
     dX[8] = (1/m)*.5*C_A*rho*A*X[8]^2 - mu*X[1]/X[1]^3; # Spherical Planet
     dX[9] = (1/m)*.5*C_S*rho*A*X[9]^2;
     dX[10] = -(1/m)*.5*C_N*rho*A*X[10]^2;
+
     # Angular Acceleration
-    dX[11] = 0;
-    dX[12] = 0;
-    dX[13] = 0;
-    dX[14] = 0;
-    dX[15] = C_T*C_A*(rho*(sqrt(X[1]^2 + X[2]^2 + X[3]^2))/2)
+    dX[11:13] = inv(J)*(Torque - cross( X[11:13], J*X[11:13] ));
+    dX[14] = C_T*C_A*(rho*(sqrt(X[1]^2 + X[2]^2 + X[3]^2)^3)/2);
 end
--- EntryVehicleWrapper.jl
+++ EntryVehicleWrapper.jl
@@ -1,15 +1,24 @@
 using DifferentialEquations
+using LinearAlgebra
+
 include("EntryVehicleDynamics.jl")
 # Dummy Initial Conditions
-X0 = zeros(15)
-X0[1] = 100000.0;
-X0[2] = 1000.0;
-X0[3] = 1000.0;
-X0[8] = 10.0;
-X0[9] = 10.0;
-X0[10] = 10.0;
+    X0 = zeros(14)
+    X0[1] = 100000.0;
+    X0[2] = 0;
+    X0[3] = 0;
+    X0[4] = 1;
+    X0[5] = 0;
+    X0[6] = 0;
+    X0[7] = 0;
+    X0[8] = 1000.0;
+    X0[9] = 0;
+    X0[10] = 0;
+    X0[11] = 0;
+    X0[12] = 0;
+    X0[13] = 0;
+    X0[14] = 300;
 
-
-tspan = (0.0,10.0)
+tspan = (0.0,5.0)
 prob = ODEProblem(entry_vehicle!, X0, tspan)
 sol = solve(prob)
